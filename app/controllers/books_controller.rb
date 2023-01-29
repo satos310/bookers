68ccf7387526ecf5cb9328438350adde77a31f2e
satos310 <119788681+satos310@users.noreply.github.com>
2023-01-29 08:06:10 +0000

    [fix index page]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,34 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.new
+  end
+  
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to '/top'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @books = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  def destroy
+    book = book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   
-  config.hosts << "0019ed8a3c0f4ed1b4ddf201f87527e9.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
